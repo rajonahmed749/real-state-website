ee713cb3bdac92675e7d2074279fea96cdb3fd0a
rajonahmed749 <raazon749@gmail.com>
2022-08-15 23:17:54 +0600

    env file added

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,8 +5,9 @@ export const baseUrl = 'https://bayut.p.rapidapi.com'
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
-            'X-RapidAPI-Key': process.env.SECRET_KEY,
-            'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+            'X-RapidAPI-Key': process.env.SECRET_KEY
+            
         }
     })
     return data;
