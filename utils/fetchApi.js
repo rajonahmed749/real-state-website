8dd6f9f3db7c4547611f6b5f02ce337096cd795d
rajonahmed749 <raazon749@gmail.com>
2022-08-15 22:43:40 +0600

    first commit

---
+++ utils/fetchApi.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+export const fetchApi = async (url) => {
+    const { data } = await axios.get((url), {
+        headers: {
+            'X-RapidAPI-Key': 'bef9dab5c5msh3a4dbc44a547a4fp1bbf06jsna5de2a045911',
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+        }
+    })
+    return data;
+}
